796e37cef3c4785100033cd5a3427019ad390cf9
Sanford Staab <sanfords@msn.com>
2022-10-28 03:57:24 -0700

    integrate my notes.html into the site

--- .eleventy.js
+++ .eleventy.js
@@ -38,6 +38,9 @@ module.exports = function (eleventyConfig) {
   // Copy favicon to route of /_site
   eleventyConfig.addPassthroughCopy("./src/favicon.ico");
 
+  // Copy notes.html to route of /_site
+  eleventyConfig.addPassthroughCopy(".src/static/notes.html");
+
   // Minify HTML
   eleventyConfig.addTransform("htmlmin", function (content, outputPath) {
     // Eleventy 1.0+: use this.inputPath and this.outputPath instead
