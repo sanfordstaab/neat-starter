ed54f88bac2639146f57b398ca126c9508d23caa
Sanford Staab <sanfords@msn.com>
2022-11-01 02:09:55 -0700

    rename style.css to tailwind-generated-style.css and add test code to Alpine-Demo.html to show build problems

--- .eleventy.js
+++ .eleventy.js
@@ -26,10 +26,9 @@ module.exports = function (eleventyConfig) {
 
   // Copy Static Files to /_Site
   eleventyConfig.addPassthroughCopy({
-    "./src/admin/config.yml": "./admin/config.yml",
-    "./node_modules/alpinejs/dist/cdn.min.js": "./static/js/alpine.js",
-    "./node_modules/prismjs/themes/prism-tomorrow.css":
-      "./static/css/prism-tomorrow.css",
+    "./src/admin/config.yml":                           "./admin/config.yml",
+    "./node_modules/alpinejs/dist/cdn.min.js":          "./static/js/alpine.js",
+    "./node_modules/prismjs/themes/prism-tomorrow.css": "./static/css/prism-tomorrow.css",
   });
 
   // Copy Image Folder to /_site
